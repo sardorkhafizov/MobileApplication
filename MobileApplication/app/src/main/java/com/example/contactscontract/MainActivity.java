f0e442a897b9b73a674de0cb74534cfde1875349
PC-1060 <sar4i@mail.ru>
2023-07-18 23:18:00 +0500

    finished project

---
+++ MobileApplication/app/src/main/java/com/example/contactscontract/MainActivity.java
@@ -0,0 +1,223 @@
+package com.example.contactscontract;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.os.Build;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.provider.ContactsContract;
+import android.telephony.SmsManager;
+import android.view.View;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.contactscontract.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    public static final int REQUEST_READ_CONTACTS = 79;
+    private static final int REQUEST_CODE_PERMISSION = 2;
+
+    ListView list;
+    String phone;
+    ArrayList mobileArray;
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 0;
+    String mPermission = Manifest.permission.ACCESS_FINE_LOCATION;
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_CONTACTS)
+                == PackageManager.PERMISSION_GRANTED) {
+            mobileArray = getAllContacts();
+        } else {
+            requestPermission();
+        }
+
+        try {
+            PackageManager MockPackageManager = null;
+            if (ActivityCompat.checkSelfPermission(this, mPermission)
+                    != MockPackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(this, new String[]{mPermission},
+                        REQUEST_CODE_PERMISSION);
+
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        list = findViewById(R.id.list);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, mobileArray);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), "1",
+                        Toast.LENGTH_LONG).show();            }
+        });
+
+        list.setOnItemClickListener((adapterView, view, i,l) -> {
+            Object object = adapterView.getAdapter().getItem(i);
+            Toast.makeText(getApplicationContext(), object.toString(),
+                    Toast.LENGTH_LONG).show();
+            sendSMSMessage(object.toString());
+        });
+    }
+    GPSTracker gps;
+
+
+    protected void sendSMSMessage(String phone) {
+//        phoneNo = txtphoneNo.getText().toString();
+//        message = txtMessage.getText().toString();
+        this.phone = phone;
+        Toast.makeText(getApplicationContext(), phone,
+                Toast.LENGTH_LONG).show();
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.SEND_SMS)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.SEND_SMS},
+                        MY_PERMISSIONS_REQUEST_SEND_SMS);
+            }
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.SEND_SMS},
+                    MY_PERMISSIONS_REQUEST_SEND_SMS);
+        }
+    }
+
+    private void requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.READ_CONTACTS)) {
+            // show UI part if you want here to show some rationale !!!
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_CONTACTS},
+                    REQUEST_READ_CONTACTS);
+        }
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.READ_CONTACTS)) {
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_CONTACTS},
+                    REQUEST_READ_CONTACTS);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case REQUEST_READ_CONTACTS: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mobileArray = getAllContacts();
+                } else {
+                    // permission denied,Disable the
+                    // functionality that depends on this permission.
+                }
+                break;
+            }
+
+            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    gps = new GPSTracker(MainActivity.this);
+
+                    // check if GPS enabled
+                    if(gps.canGetLocation()){
+
+                        double latitude = gps.getLatitude();
+                        double longitude = gps.getLongitude();
+
+                        SmsManager smsManager = SmsManager.getDefault();
+                        smsManager.sendTextMessage(phone, null, " latitude: "+latitude + " longitude: " + longitude, null, null);
+                        Toast.makeText(getApplicationContext(), "SMS sent.",
+                                Toast.LENGTH_LONG).show();
+
+                    }else{
+                        // can't get location
+                        // GPS or Network is not enabled
+                        // Ask user to enable GPS/network in settings
+                        gps.showSettingsAlert();
+                    }
+
+
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "SMS faild, please try again.", Toast.LENGTH_LONG).show();
+                    break;
+                }
+            }
+        }
+    }
+
+
+    @SuppressLint("Range")
+    private ArrayList getAllContacts() {
+        ArrayList<String> nameList = new ArrayList<>();
+        ContentResolver cr = getContentResolver();
+        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
+                null, null, null, null);
+        if ((cur != null ? cur.getCount() : 0) > 0) {
+
+            while (cur.moveToNext()) {
+
+                String id = cur.getString(
+                        cur.getColumnIndex(ContactsContract.Contacts._ID));
+
+//                String number = cur.getString(cur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+//                String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.NU));
+
+//                nameList.add(number);
+
+                if (cur.getInt(cur.getColumnIndex( ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0) {
+                    Cursor pCur = cr.query(
+                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                            null,
+                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                            new String[]{id}, null);
+                    while (pCur.moveToNext()) {
+                        String phoneNo = pCur.getString(pCur.getColumnIndex(
+                                ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        nameList.add(phoneNo);
+                    }
+                    pCur.close();
+                }
+            }
+
+        }
+        if (cur != null) {
+            cur.close();
+        }
+        return nameList;
+    }
+}
